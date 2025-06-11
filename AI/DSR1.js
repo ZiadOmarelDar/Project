3af2e2a3439a843bb70faa3b3d3b8e488888fa12
Mahmoud Alsoudy <113215972+Mahmoudshar0@users.noreply.github.com>
2025-06-11 23:25:10 +0300

    updates

--- AI/DSR1.js
+++ AI/DSR1.js
@@ -1,6 +1,6 @@
 // const InferenceClient = require("@huggingface/inference");
 
-// const client = new InferenceClient("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");
+// const client = new InferenceClient("");
 
 // const chatCompletion = async () => await client.chatCompletion({
 //     provider: "novita",
@@ -17,7 +17,7 @@
 
 // const InferenceClient = require("@huggingface/inference");
 
-// const client = new InferenceClient("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");
+// const client = new InferenceClient("");
 
 // const chatCompletion = async () => {
 //     return await client.chatCompletion({
@@ -43,7 +43,7 @@
 
 // const { HfInference } = require('@huggingface/inference');
 
-// const client = new HfInference("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");
+// const client = new HfInference("");
 
 // (async () => {
 //     const response = await client.chatCompletion({
@@ -66,7 +66,7 @@
 // 		"https://router.huggingface.co/replicate/v1/models/black-forest-labs/flux-dev/predictions",
 // 		{
 // 			headers: {
-// 				Authorization: `Bearer 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`,
+// 				Authorization: `Bearer `,
 // 				"Content-Type": "application/json",
 // 			},
 // 			method: "POST",
@@ -90,36 +90,36 @@
 // 	console.log(`Image saved to ${filePath}`);
 // }).catch(console.error);
 
-const fetch = require('node-fetch');
-const fs = require('fs');
+// const fetch = require('node-fetch');
+// const fs = require('fs');
 
-async function query(data) {
-	const response = await fetch(
-		"https://router.huggingface.co/replicate/v1/models/black-forest-labs/flux-dev/predictions",
-		{
-			headers: {
-				Authorization: `Bearer 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`,
-				"Content-Type": "application/json",
-			},
-			method: "POST",
-			body: JSON.stringify(data),
-		}
-	);
-	const result = await response.blob();
-	return result;
-}
+// async function query(data) {
+// 	const response = await fetch(
+// 		"https://router.huggingface.co/replicate/v1/models/black-forest-labs/flux-dev/predictions",
+// 		{
+// 			headers: {
+// 				Authorization: `Bearer `,
+// 				"Content-Type": "application/json",
+// 			},
+// 			method: "POST",
+// 			body: JSON.stringify(data),
+// 		}
+// 	);
+// 	const result = await response.blob();
+// 	return result;
+// }
 
-query({
-	input: {
-		prompt: "Astronaut riding a horse",
-	},
-}).then((blob) => {
-	// Save blob to a file
-	const fileStream = fs.createWriteStream("output.jpg");
-	blob.stream().pipe(fileStream);
-	fileStream.on("finish", () => {
-		console.log("Image saved to output.jpg");
-	});
-}).catch((err) => {
-	console.error("Error fetching image:", err);
-});
+// query({
+// 	input: {
+// 		prompt: "Astronaut riding a horse",
+// 	},
+// }).then((blob) => {
+// 	// Save blob to a file
+// 	const fileStream = fs.createWriteStream("output.jpg");
+// 	blob.stream().pipe(fileStream);
+// 	fileStream.on("finish", () => {
+// 		console.log("Image saved to output.jpg");
+// 	});
+// }).catch((err) => {
+// 	console.error("Error fetching image:", err);
+// });
